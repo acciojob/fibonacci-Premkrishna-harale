1db4e91beb44f1cb5181c073a8ff3375e0ad254d
Premkrishna-harale <150167484+Premkrishna-harale@users.noreply.github.com>
2024-04-17 13:57:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	function fibonacci(num) {
+    if (num <= 0) return "Invalid input"; // Handling invalid input
+    
+    let fibArray = [0, 1];
+    
+    for (let i = 2; i <= num; i++) {
+        fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    }
+    
+    return fibArray[num - 1];
+}
+
+// Test cases
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
+
 }
 
 module.exports = fibonacci;
